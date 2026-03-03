6adad0e4f5d67211d5ae84588fbdf9605eb397c3
AlexKeso7 <alex.liulundstrom@elev.ga.lbs.se>
2026-03-03 14:35:43 +0100

    Corruption imminent vro

--- Alucard - vt26/Assets/Keso_Scripts/HealthManager.cs
+++ Alucard - vt26/Assets/Keso_Scripts/HealthManager.cs
@@ -8,39 +8,24 @@ public class HealthManager : MonoBehaviour
     public int maxHealth = 100;
     public int currentHealth;
 
-    public HealthBar healthBar;
-    public GameObject bloodEffect;
 
     private void Start()
     {
         currentHealth = maxHealth;
-        healthBar.SetMaxHealth(maxHealth);
     }
 
 
-    public void TakeDamage(int damage, Vector2 origin)
+    public void HurtPlayer(int damageToGive)
     {
-        currentHealth -= damage;
+        currentHealth -= damageToGive;
 
-        // Blood Particle example
-        Instantiate(bloodEffect, transform.position, Quaternion.identity);
-
-        // Camera shake code example
-        //CameraShake.instance.Shake();
-
-        // Knockback code example
-        
 
         if (currentHealth <= 0)
         {
             Destroy(gameObject);
         }
 
-        healthBar.SetCurrentHealth(currentHealth);
     }
 
-    internal void HurtPlayer(int damageToGive)
-    {
-        throw new NotImplementedException();
-    }
+
 }
