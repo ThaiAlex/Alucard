ba53962dfb2261f865cd96eb3a1c0ff58938fbea
Svan008 <sagaahlsen@gmail.com>
2026-03-04 15:43:12 +0100

    parallax

---
+++ Alucard - vt26/Assets/Saga scripts/parallax.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallax : MonoBehaviour
+{
+    private float length, startpos; // length finns
+    public GameObject cam; //kamera finns
+    public float parallaxEffect; // parallax effekt finns
+
+    void Start() // När spelet startar
+    {
+        startpos = transform.position.x; //kamerans startposition
+        length = GetComponent<SpriteRenderer>().bounds.size.x; // längd på hur långt bilderna renderas
+    }
+
+    void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));  // 
+        float dist = (cam.transform.position.x * parallaxEffect); //
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z); //
+
+        if (temp > startpos + length) startpos += length; //
+        else if (temp < startpos - length) startpos -= length; // 
+    }
+}
+// Saga
