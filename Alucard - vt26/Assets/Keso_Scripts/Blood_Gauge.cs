d6ebc0aeb1810d036c6d6db33ec2f21aa6b86cd7
Mzinho141 <telmuun.endgerel@elev.ga.lbs.se>
2026-03-11 11:06:22 +0100

    merge conflict fixed

--- Alucard - vt26/Assets/Keso_Scripts/Blood_Gauge.cs
+++ Alucard - vt26/Assets/Keso_Scripts/Blood_Gauge.cs
@@ -2,43 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< Updated upstream
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
-public class Blood_Gauge : MonoBehaviour
-{
-    public int maxBlood;
-    public int currentBlood;
-    public 
-=======
 
 public class Blood_Gauge : MonoBehaviour
 {
     public int maxBlood = 20;
     public int currentBlood = 0;
     public float boostSpeed = 8f;
->>>>>>> Stashed changes
-
 
     public void Start()
     {
-<<<<<<< Updated upstream
-        Blood.maxValue = maxEXP;
-        expSlider.value = currentEXP;
-    }
-
-    public void GainEXP(int amount)
-    {
-        currentEXP += amount;
-
-        if (currentEXP > maxEXP)
-            currentEXP = maxEXP;
-
-        expSlider.value = currentEXP;
-    }
-
-=======
         FindFirstObjectByType<Movement>();
     }
 
@@ -46,15 +20,9 @@ public class Blood_Gauge : MonoBehaviour
     {
         currentBlood += amount;
 
-        if(currentBlood > maxBlood)
+        if (currentBlood > maxBlood)
         {
             currentBlood = maxBlood;
-            
         }
-
     }
-
-
-
->>>>>>> Stashed changes
 }
